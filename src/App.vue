c8af548e1501ff81e5260036aa3cfbcd9efe4201
HikariLight <merzougui.dhia@gmail.com>
2021-03-19 10:58:08 +0100

    Separated components

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,13 @@
 <template>
     <h1>Procrastination Station</h1>
-    
-    <form @submit.prevent="addEntry">
-      <label>New Entry</label>
-      <input v-model="newEntry" name="entry">
-      <button>Add Entry</button>
-    </form>
-
-    <ul>
-      <li v-for="(entry, index) in entryList" :key="index">
-        <h3>{{entry.todo}}</h3>
-      </li>
-    </ul>
-
-    <h4 v-if="entryList.length === 0">Nothing here</h4>
+    <ToDoList />
 </template>
 
 <script>
-import { ref } from "vue";
+import ToDoList from './components/ToDoList.vue'
 
-export default {
-  setup(){
-      localStorage.clear();
-      
-			const newEntry = ref("");
-			const defaultData = [{
-				done: false,
-				todo: ""
-			}]
-
-			const entryData = JSON.parse(localStorage.getItem('entryList')) || defaultData;
-			const entryList = ref(entryData);
-      
-			function addEntry () {
-				if (newEntry.value) {
-					entryList.value.push({
-						done: false,
-						todo: newEntry.value
-					});
-					newEntry.value = "";
-				}
-				saveData();
-			}
-
-			function saveData () {
-				const storageData = JSON.stringify(entryList.value);
-				localStorage.setItem('entryList', storageData);
-			}
-
-			return {
-				entryList,
-				newEntry,
-				addEntry,
-				saveData
-			}
-    }
+export default{
+  components: { ToDoList }
 }
 
 </script>
@@ -67,22 +20,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-label{
-  padding: 0.5em;
-}
-
-input{
-  padding: 1em;
-}
-
-button{
-  padding: 10px;
-  margin: 1em;
-  border-radius: 5px;
-  border: #FFFFFF;
-  background-color: #14971F;
-  color: #FFFFFF;
 }
 </style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,77 @@
+<template>    
+    <form @submit.prevent="addEntry">
+      <label>New Entry</label>
+      <input v-model="newEntry" name="entry">
+      <button>Add Entry</button>
+    </form>
+
+    <ul>
+      <li v-for="(entry, index) in entryList" :key="index">
+		<input type="checkbox" id="checkbox">
+        <label>{{entry.todo}}</label>
+      </li>
+    </ul>
+
+    <h4 v-if="entryList.length === 0">Nothing here</h4>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+	setup(){
+		localStorage.clear();
+		const newEntry = ref("");
+		const defaultData = [{
+			done: false,
+			todo: ""
+		}]
+
+		const entryData = JSON.parse(localStorage.getItem('entryList')) || defaultData;
+		const entryList = ref(entryData);
+		
+		function addEntry () {
+			if (newEntry.value) {
+				entryList.value.push({
+					done: false,
+					todo: newEntry.value
+				});
+				newEntry.value = "";
+			}
+			saveData();
+		}
+
+		function saveData () {
+			const storageData = JSON.stringify(entryList.value);
+			localStorage.setItem('entryList', storageData);
+		}
+
+		return {
+			entryList,
+			newEntry,
+			addEntry,
+			saveData
+		}
+	}
+}
+
+</script>
+
+<style scoped>
+label{
+  padding: 0.5em;
+}
+
+input{
+  padding: 1em;
+}
+
+button{
+  padding: 10px;
+  margin: 1em;
+  border-radius: 5px;
+  border: #FFFFFF;
+  background-color: #14971F;
+  color: #FFFFFF;
+}
+</style>
